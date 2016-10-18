9652a1146d8568d9eb352b2a31d96b7bdfad22e2
hjlynch2 <harris.lynch16@gmail.com>
2016-10-18 00:00:59 -0500

    created basic user login functionality

--- speedSearch.py
+++ speedSearch.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
@@ -14,6 +14,63 @@ app.config['MYSQL_HOST'] = 'fa16-cs411-10.cs.illinois.edu'
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/LogIn', methods=['GET','POST'])
+def login():
+    invalid = False
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        query = 'SELECT * FROM Users WHERE username = "' + username + '" AND password = "' + password + '" LIMIT 1'
+        cur = mysql.connection.cursor()
+        cur.execute(query)
+        validLogin = cur.rowcount
+        cur.close()
+        if validLogin: # valid credentials
+            return redirect(url_for('game', username = username))
+        else:
+            invalid = True
+    return render_template('login.html', invalid = invalid)
+
+
+@app.route('/CreateAccount', methods=['GET','POST'])
+def createAccount():
+    alreadyExists = False
+    invalidPassword = False
+    cur = mysql.connection.cursor()
+    conn = mysql.connection
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if len(password) < 6:
+            invalidPassword = True
+        query = 'SELECT * FROM Users WHERE username = "' + username + '"'
+        cur.execute(query)
+        alreadyExists = cur.rowcount
+        if not (alreadyExists or invalidPassword):
+            query = 'INSERT INTO Users (username, password, admin) VALUES ("' + username + '","' + password + '",0)'
+            cur.execute(query)
+            conn.commit()
+            cur.close()
+            return redirect(url_for('game', username = username))
+    cur.close()
+    return render_template('createAccount.html', alreadyExists = alreadyExists, invalidPassword = invalidPassword)
+
+
+@app.route('/Game', methods=['GET','POST'])
+def game():
+    username = request.args['username']
+    query = 'SELECT admin FROM Users WHERE username = "' + username + '"'
+    cur = mysql.connection.cursor()
+    cur.execute(query)
+    admin = (cur.fetchone())[0]
+    cur.close()
+    return render_template('game.html', username = username, admin = admin)
+
+@app.route('/Admin', methods=['GET','POST'])
+def admin():
+    return render_template('admin.html')
 
 
 @app.route('/select')
