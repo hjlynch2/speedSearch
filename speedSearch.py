684312c52c686861dbf310ef070cfdce56e363cd
hjlynch2 <harris.lynch16@gmail.com>
2016-10-17 21:40:53 -0500

    first query

--- speedSearch.py
+++ speedSearch.py
@@ -1,23 +1,28 @@
 from flask import Flask, render_template
-#from flask_mysqldb import MySQL
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-'''
+
 mysql = MySQL(app)
-app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_USER'] = 'wcrasta2'
+app.config['MYSQL_PASSWORD'] = 'password' 
 app.config['MYSQL_DB'] = 'SpeedSearch'
 app.config['MYSQL_HOST'] = 'fa16-cs411-10.cs.illinois.edu'
-'''
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/<name>')
-def user(name):
-    return render_template('user.html', name=name)
+@app.route('/select')
+def select():
+    cur = mysql.connection.cursor()
+    cur.execute('''SELECT * FROM testing''')
+    rv=cur.fetchall()
+    cur.close()
+    return str(rv)
 
 if __name__ == '__main__':
     app.run(debug=True)
